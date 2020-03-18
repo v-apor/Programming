8329464d25de3e33b1831ef54fff210a375dffea
Apoorv <vaporinside@gmail.com>
2020-03-18 09:53:11 +0530

    Testing what happens if diffrent class and file name and changes because of public specifier in naming convention

--- Java/HelloWorld.java
+++ Java/HelloWorld.java
@@ -1,5 +1,11 @@
-class HelloWorld{
+public class HelloWorld{
     public static void main(String[] args){
         System.out.println("Hello World!!");
+    }
+}
+
+class Goodbye{
+    public static void main(String[] args){
+        System.out.println("Goodbye");
     }
 }
