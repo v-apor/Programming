393aced59ff7dfad30fc5426d4cbddce4807c278
Apoorv <vaporinside@gmail.com>
2020-03-25 08:47:21 +0530

    Learned about taking multiple input and also casting it to integer >Input.java

--- Java/Input.java
+++ Java/Input.java
@@ -5,6 +5,8 @@ class Input{
         System.out.println("Enter your name: ");
         Scanner scObj = new Scanner(System.in);
         String inputStr = scObj.nextLine();
-        System.out.printf("Your name is %s",inputStr);
+        System.out.println("Enter your age: ");
+        int inputAge = Integer.parseInt(scObj.nextLine());
+        System.out.printf("Your name is %s\nAnd you will be %d years old in 2025",inputStr, inputAge+5);
     }
 }
