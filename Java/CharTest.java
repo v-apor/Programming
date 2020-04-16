6a39c7bf92e1b59c9763fc12c9e8f8152f788b89
Apoorv <vaporinside@gmail.com>
2020-04-16 12:55:29 +0530

    char<->int casting

---
+++ Java/CharTest.java
@@ -0,0 +1,7 @@
+class CharTest{
+    public static void main(String[] args){
+        String name = "apoorv";
+        System.out.println((int)name.charAt(0));
+        System.out.println((char)100);
+    }
+}
