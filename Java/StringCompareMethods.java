7d9582d2f3313d87821d7e0c408f594d58b94e04
Apoorv <vaporinside@gmail.com>
2020-06-10 08:49:55 +0530

    Java String methods (contains() and indexOf()) and Python Exceptions

---
+++ Java/StringCompareMethods.java
@@ -0,0 +1,10 @@
+public class StringCompareMethods{
+    public static void main(String[] args){
+        String vovels = "aeiouAEIOU";
+        System.out.println(vovels.contains("A"));
+        System.out.println(vovels.contains("z"));
+        System.out.println(vovels.indexOf('a'));
+        System.out.println(vovels.indexOf('E'));
+        System.out.println(vovels.indexOf('Z'));
+    }
+}
---
+++ Python/Error-Exception/ExceptionTest.py
@@ -0,0 +1,4 @@
+try:
+    print(0/0)
+except:
+    print("error occured")
