9476c7dd159fcb19dc7769c48e0680a13cc5ea4d
Apoorv <vaporinside@gmail.com>
2020-10-31 22:30:32 +0530

    GoLang Datatypes

---
+++ Go/datatype.go
@@ -0,0 +1,12 @@
+package main
+import "fmt"
+
+func main(){
+	var name string = "Temp"
+	name = "Apoorv Chandrakar"
+	var age int = 23
+	var height float32 = 182.5
+	fmt.Printf("Name: %s",name)
+	fmt.Printf("\nAge: %d",age)
+	fmt.Printf("\nHeight: %f",height)
+}
