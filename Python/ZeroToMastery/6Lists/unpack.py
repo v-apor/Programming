ba19ba83eb8d69fe70eedb5f556a167483224e4e
Apoorv <vaporinside@gmail.com>
2021-02-18 19:11:50 +0530

    Python Basics Dictionaries

---
+++ Python/ZeroToMastery/6Lists/unpack.py
@@ -0,0 +1,14 @@
+a, b, c = [1,2,3]
+
+print(a)
+print(b)
+print(c)
+
+a, b, c, *others, d = [1,2,3,4,5,6,7,8,9]
+
+print(a)
+print(b)
+print(c)
+print(others)
+print(d)
+
---
+++ Python/ZeroToMastery/7Dict/dict.py
@@ -0,0 +1,11 @@
+# It's hashmap implementation in python
+
+dictionary = {
+    'name': 'vapor',
+    'age': 24,
+    'marks': [50,75,90,40]
+}
+
+print(dictionary)
+print(dictionary['name'])
+print(dictionary['marks'][1])
