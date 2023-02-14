ee76dba933a00a9aadcb6bd4e9516a3b68bb145d
v-apor <apoorv.learn@gmail.com>
2023-02-14 18:21:23 -0500

    JavaScript | Arrow Functions and Scope

---
+++ JavaScript/CS546/arrrow.js
@@ -0,0 +1,3 @@
+let fun = (x, y) => x + y;
+
+console.log(fun(14, 61));
---
+++ JavaScript/CS546/scope.js
@@ -0,0 +1,12 @@
+let a = 14;
+var b = 61;
+
+function test() {
+    let x = b * 4;
+    console.log(a);
+    console.log(b);
+}
+
+test();
+
+console.log(typeof null);
